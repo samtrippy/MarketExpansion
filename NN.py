f990f42a0e51b1149723ea44d7374030f898e654
strippy16 <samanthatrippy2016@u.northwestern.edu>
2015-05-21 12:43:03 -0500

    Finished the k-NN classifier
    Takes input for a proposed business: location (latitude and longitude)
    and categories (e.g. "Food"). also takes the value k you want for the
    k-NN classifier.
    
    Returns the expected # of stars

--- NN.py
+++ NN.py
@@ -4,10 +4,13 @@ import json, math
 import Queue
 from pprint import pprint
 
-def NearestNeighbor(iLat, iLon, iCats):
+def NearestNeighbor(iLat, iLon, iCats, kNN):
+    """takes the latitude, longitude, and categories for a proposed business as well as a value
+    k to generate a k-Nearest Neighbot classifier, identifying the most similar existing
+    businesses and using their info to predict the success of a future venture"""
     with open("yelp_academic_dataset_business.json") as json_data:
-        nearest = [float("inf")] * 10
-        closestBusinesses = [None] * 10
+        nearest = [float("inf")] * kNN
+        closestBusinesses = [None] * kNN
         sharedCats = [] 
         ret = []    #list of businesses we'll finally return
         for line in json_data:          #for each business
@@ -15,58 +18,41 @@ def NearestNeighbor(iLat, iLon, iCats):
             latitude = float(data['latitude'])  #get lat
             longitude = float(data['longitude'])    #get long
             distance = math.sqrt(math.pow((latitude - iLat),2) + math.pow((longitude - iLon),2))
-
-#new shit
-            allCats = data["categories"]
+            
+            allCats = data["categories"]    #a business' categoires
             for cat in allCats:
                 if cat in iCats and cat not in sharedCats:
-                    sharedCats.append(data)
+                    sharedCats.append(data) #append shared ones
                 else:
                     pass
-            sim = len(sharedCats)
-            
+            sim = len(sharedCats)   # num. similar
+            #need to keep smallest distance and businesses separate
             m = max(nearest)
             i = nearest.index(m)
             if sim >= 1:
-                final = float(1 - (sim/((len(iCats))+(len(allCats)))))
-                if m > final:
-                    nearest.remove(nearest[i])
-                    nearest.append(data)
+                result = 200*float(1 - (sim/float(((len(iCats))+(len(allCats))))))
+                final = distance + result
+                if final < m:       #if it's better than any in the list
+                    nearest.remove(m)
+                    nearest.append(final)
+                    closestBusinesses.pop(i)
+                    closestBusinesses.append(data)
                 else:
                     pass
             sharedCats = []
             distance = 0
-        pprint(nearest)
-#        for x in nearest:
-#            print(x["name"])
+            result = 0
+        print(nearest)  #the "distance" values incorporating shared categories
+        pprint(closestBusinesses) #pretty prints the businesses as dictionaries
+        totStars = float(0)
+        for bus in closestBusinesses:
+            totStars += bus["stars"]
+        expectedStars = totStars/float(kNN) #calculate avg num of stars of k-Nearest Neighbors
+        print(expectedStars)
+        return expectedStars
 
-            
-##            m = max(shortestDistance)   #max in list
-##            i = shortestDistance.index(m)   #index of the max
-##            if m > distance:
-##                shortestDistance.remove(m)  #remove the bigger distance
-##                closestBusinesses.pop(i)      #remove the corresponding business
-##                shortestDistance.append(distance)   #add new distance
-##                closestBusinesses.append(data)  #add corresponding business
-##            else:
-##                pass
-##            distance = 0
-##        sharedCats = [] 
-##        ret = []    #list of businesses we'll finally return
-##        for item in closestBusinesses:
-##            allCats = item["categories"]
-##            for cat in allCats:
-##                if cat in iCats and cat not in sharedCats:
-##                    sharedCats.append(item)
-##                else:
-##                    pass
-##            sim = len(sharedCats)
-##            if sim >= 1:
-##                final = float(1 - (sim/((len(iCats))+(len(allCats)))))
-##                print(final)
-##                ret.append(item)
-##                sharedCats = []
-##        for x in ret:
-##            print(x["name"])
+#test for food in AZ
+#NearestNeighbor(33.499313000000001, -111.98375799999999,["Food"], 10)
 
-NearestNeighbor(33.499313000000001, -111.98375799999999,["Food"])
+#test for Restaurants in Las Vegas
+NearestNeighbor(36.095424000000001, -115.175922, ["Restaurants"], 5)
